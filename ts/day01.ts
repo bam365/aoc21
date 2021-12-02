8151028b51b985ee214111f947681f2fb385482b
Blake Mitchell <bam365@umsystem.edu>
2021-12-01 21:27:28 -0600

    I like this better

--- ts/day01.ts
+++ ts/day01.ts
@@ -1,25 +1,18 @@
+import { pipe } from "fp-ts/lib/function";
 import * as ROArray from "fp-ts/ReadonlyArray"
 
 import * as aoclib from "./aoclib"
 
 
 const main = () => {
-    const numbers = aoclib.readNumbers();
-    if (numbers.length === 0) {
-        console.log("Not enough numbers")
-    } else {
-        const answer = countIncreases(numbers[0], 0, ROArray.dropLeft(1)(numbers))
-        console.log(answer)
-    }
-}
-
-const countIncreases = (prevN: number, acc: number, xs: readonly number[]): number => {
-    if (xs.length === 0) {
-        return acc
-    } else {
-        const newAcc = xs[0] > prevN ? acc + 1 : acc;
-        return countIncreases(xs[0], newAcc, ROArray.dropLeft(1)(xs))
-    }
+    const numbers = aoclib.readNumbers()
+    const answer = pipe(
+        numbers,
+        ROArray.zip(ROArray.dropLeft(1)(numbers)),
+        ROArray.filter(([a, b]) => a < b),
+        ROArray.size
+    )
+    console.log(answer)
 }
 
 main()
