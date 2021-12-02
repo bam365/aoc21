9356cb5ed91699853d74fad2fd9ab713260e57b3
Blake Mitchell <bam365@umsystem.edu>
2021-12-01 22:22:04 -0600

    better

--- ocaml/lib/aoccore.ml
+++ ocaml/lib/aoccore.ml
@@ -2,3 +2,11 @@ let read_ints_exn () =
   stdin
   |> CCIO.read_lines_l
   |> List.map int_of_string
+
+
+module Seq = struct
+  let count pred seq =
+    seq
+    |> CCSeq.filter pred
+    |> CCSeq.length
+end
--- ocaml/lib/day01.ml
+++ ocaml/lib/day01.ml
@@ -1,11 +1,6 @@
-let rec count_increases last_n acc = function
-  | []    -> acc
-  | x::xs -> 
-    let acc' = if x > last_n then acc + 1 else acc in
-    count_increases x acc' xs
-  
-let main () = 
-  match Aoccore.read_ints_exn () with
-  | (x::xs) -> Printf.printf "%d\n" (count_increases x 0 xs)
-  | []      -> print_endline "Not enough numbers"
-  
+let main () =
+  let numbers = Aoccore.read_ints_exn () in
+  CCSeq.drop 1 (CCList.to_seq numbers)
+  |> CCSeq.zip (CCList.to_seq numbers) 
+  |> Aoccore.Seq.count (fun (a, b) -> a < b)
+  |> Printf.printf "%d\n"
