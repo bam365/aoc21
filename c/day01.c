6694a1797a8e4b02645e8399afd8ab170a0a4ad5
Blake Mitchell <bam365@umsystem.edu>
2021-12-01 22:03:46 -0600

    more idiomatic c

--- c/day01.c
+++ c/day01.c
@@ -1,41 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* read_ints(int* size) {
-    static int INT_BUF[100000];
-    char line_buf[1024];
-    int* curr = INT_BUF;
-    
-    while (fgets(line_buf, 1024, stdin)) {
-        *curr = atoi(line_buf);
-        curr++;
-    }
-    *size = curr - INT_BUF;
-
-    return INT_BUF;
-}
-
-int count_increases(int* numbers, int size) {
-    int ret = 0;
-
-    for (int i = 1; i < size; i ++) {
-        if (numbers[i] > numbers[i - 1]) {
-            ret++;
-        }
-    }
-
-    return ret;
-}
-
 int main() {
-    int size = 0;
-    int *numbers = NULL;
+    char linebuf[1024];
+    int nprev, n;
+    int answer = 0;
 
-    numbers = read_ints(&size);
-
-    if (size < 2) {
-        puts("Not enough numbers");
-    } else {
-        printf("%d\n", count_increases(numbers, size));
+    fgets(linebuf, 1024, stdin);
+    nprev = atoi(linebuf);
+    while(fgets(linebuf, 1024, stdin)) {
+        n = atoi(linebuf);
+        if (n > nprev) {
+            answer++;
+        }
+        nprev = n;
     }
+    printf("%d\n", answer);
 }
