f42b45295f3d853f56893d705b66547857174a60
Blake Mitchell <bam365@umsystem.edu>
2021-12-04 19:29:54 -0600

    day 2 part 2 ocaml

--- ocaml/lib/day02.ml
+++ ocaml/lib/day02.ml
@@ -30,7 +30,25 @@ module ShipState = struct
     | Command.Up(i) -> { horizontal_pos; depth = depth - i }
 end
 
+module ShipStatePt2 = struct
+  type t = 
+    { horizontal_pos: int
+    ; depth: int
+    ; aim: int
+    }
+
+  let beginning = { horizontal_pos = 0; depth = 0; aim = 0 }
+
+  let run_command t = function
+    | Command.Forward(i) -> 
+      { t with horizontal_pos = t.horizontal_pos + i;  depth = t.depth + (t.aim * i) }
+    | Command.Down(i) -> { t with aim = t.aim + i }
+    | Command.Up(i) -> { t with aim = t.aim - i }
+end
+
 let main () = 
-  Aoccore.read_lines_with_parser Command.parse
-  |> CCList.fold_left ShipState.run_command ShipState.beginning
-  |> fun state -> Printf.printf "%d\n" (state.horizontal_pos * state.depth)
+  let commands = Aoccore.read_lines_with_parser Command.parse in
+  CCList.fold_left ShipState.run_command ShipState.beginning commands
+  |> fun state -> Printf.printf "pt1: %d\n" (state.horizontal_pos * state.depth);
+  CCList.fold_left ShipStatePt2.run_command ShipStatePt2.beginning commands
+  |> fun state -> Printf.printf "pt2: %d\n" (state.horizontal_pos * state.depth)
